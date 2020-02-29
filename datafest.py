bc0da94beaf935f72350e4fddd1e1a9ab6e16f1d
Robertdebrus <robertdebrus@hotmail.com>
2020-02-28 20:42:51 -0500

    update

--- datafest.py
+++ datafest.py
@@ -1,5 +1,17 @@
 import pandas as pd
+import numpy as np
 imp = pd.read_csv("C:/Users/rober/Documents/NetBeansProjects/STMarysHackathon/CompetitionDataFinal/impressions-train.csv")
 fin = pd.read_csv("C:/Users/rober/Documents/NetBeansProjects/STMarysHackathon/CompetitionDataFinal/ratings-final.csv")
 
-imp.merge(fin,left_on='rating', right_on='rkey')
+final = pd.concat([imp, fin])
+
+
+
+def get_rating(movie):
+    outl = final[final["movie"] == 14] 
+    ratings = outl[outl['rating']>=0]
+    return ratings
+
+
+#print(final.head)
+#final.to_csv(r'C:/Users/rober/Documents/NetBeansProjects/STMarysHackathon/out.csv', index=False)
